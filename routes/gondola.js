33b01bd4f4ea081c0feb9fb75554955879a3c9c2
LnkColumbus <marlonveliz.nero19@gmail.com>
2019-07-16 20:21:27 -0600

    POPULATE AL ASIGNAR GONDOLA

--- routes/gondola.js
+++ routes/gondola.js
@@ -4,6 +4,7 @@ var mdAuth = require('../middlewares/auth');
 var app = express();
 
 var Gondola = require('../models/gondola');
+var Vehicle = require('../models/vehicle');
 
 /**
  * LISTAR GONDOLAS
@@ -36,7 +37,158 @@ app.get('/', function(req, res) {
 });
 
 /**
- * ACTUALIZAR GONDOLA
+ * LISTAR GONDOLAS DISPONIBLES
+ */
+
+app.get('/availables', function(req, res) {
+
+    Gondola.find({ _truck: { $eq: null } })
+        .sort({ plate: 'asc' })
+        .exec(
+            function(err, gondolas) {
+
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al buscar gondola',
+                        errors: err
+                    });
+                }
+
+                res.status(200).json({
+                    ok: true,
+                    gondolas: gondolas
+                });
+
+            });
+
+});
+
+/**
+ * ASIGNAR GONDOLA
+ */
+
+app.put('/asignar/:id', mdAuth.verificaToken, function(req, res) {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    Gondola.findById(id)
+        .then(function(gondola) {
+            if (!gondola) {
+                res.status(400).json({
+                    ok: false,
+                    mensaje: 'La gondola con el id' + id + ' no existe',
+                    errors: { message: 'No existe una gondola con ese ID' }
+                });
+            }
+
+            gondola._truck = body._truck._id;
+
+            gondola.save()
+                .then(function(gondolaA) {
+
+                    Vehicle.findByIdAndUpdate({ _id: body._truck._id }, { $set: { _gondola: gondolaA._id } })
+                        .then(function(vehicleG) {
+
+                            gondolaA.populate('_truck', function(err, gondolaPop) {
+
+                                res.status(200).json({
+                                    ok: true,
+                                    gondola: gondolaPop
+                                });
+                            });
+
+                        })
+                        .catch(function(err) {
+                            res.status(400).json({
+                                ok: false,
+                                mensaje: 'Error al asignar gondola al vehiculo',
+                                errors: err
+                            });
+                        });
+                })
+                .catch(function(err) {
+                    res.status(400).json({
+                        ok: false,
+                        mensaje: 'Error al asignar vehiculo a la gondola',
+                        errors: err
+                    });
+                });
+        })
+        .catch(function(err) {
+            res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar gondola',
+                errors: err
+            });
+        });
+
+
+});
+
+/**
+ * DESASIGNAR GONDOLA
+ */
+
+app.put('/desasignar/:id', mdAuth.verificaToken, function(req, res) {
+
+    var id = req.params.id;
+    var body = req.body;
+
+    console.log(id);
+
+    Gondola.findById(id)
+        .then(function(gondola) {
+            if (!gondola) {
+                res.status(400).json({
+                    ok: false,
+                    mensaje: 'La gondola con el id' + id + ' no existe',
+                    errors: { message: 'No existe una gondola con ese ID' }
+                });
+            }
+
+            Vehicle.findByIdAndUpdate(gondola._truck._id, { $set: { _gondola: body._truck._id } })
+                .then(function(vehicleG) {
+                    gondola._truck = body._truck._id;
+
+                    gondola.save()
+                        .then(function(gondolaV) {
+                            res.status(200).json({
+                                ok: true,
+                                gondola: gondolaV
+                            });
+                        })
+                        .catch(function(err) {
+                            res.status(400).json({
+                                ok: false,
+                                mensaje: 'Error al quitar vehiculo a la gondola',
+                                errors: err
+                            });
+                        });
+                })
+                .catch(function(err) {
+                    res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al quitar gondola al vehiculo',
+                        errors: err
+                    });
+
+                });
+        })
+        .catch(function(err) {
+            res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar gondola',
+                errors: err
+            });
+        });
+
+
+});
+
+/**
+ * BORRAR GONDOLA
  */
 
 app.put('/delete/:id', mdAuth.verificaToken, function(req, res) {
@@ -90,7 +242,7 @@ app.put('/:id', mdAuth.verificaToken, function(req, res) {
             }
 
             gondola.plate = body.plate;
-            gondola._truck = body._truck;
+            gondola._truck = body._truck._id;
             gondola.basics = body.basics;
             gondola.pits = body.pits;
 

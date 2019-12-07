7983fafdff0b294fed51ccbf8fe4238f290edbcc
LnkColumbus <marlonveliz.nero19@gmail.com>
2019-12-07 12:11:53 -0600

    ELIMINAR REPORTE CUADROS

--- routes/greenTrip.js
+++ routes/greenTrip.js
@@ -7,7 +7,6 @@ var GreenTrip = require('../models/greenTrip');
 var Vehicle = require('../models/vehicle');
 var Gondola = require('../models/gondola');
 
-
 /**
  * LISTAR REPORTES CUADROS
  */
@@ -73,7 +72,7 @@ app.get('/reports', function(req, res) {
         $project: {
             _id: '$_id',
             _employee: { _id: 1, name: 1 },
-            _type: { _id: 1, name: 1 },
+            _type: { _id: 1, name: 1, km: 1 },
             _vehicle: { _id: 1, type: 1, plate: 1, mts: 1, km: 1 },
             _material: { _id: 1, code: 1, name: 1 },
             date: '$date',
@@ -96,6 +95,140 @@ app.get('/reports', function(req, res) {
             viajesV: reports
         });
     });
+});
+
+/**
+ * LISTAR REPORTES CUADROS
+ */
+
+app.get('/group', function(req, res) {
+
+    var startDate = new Date(req.query.fecha1);
+    var endDate = new Date(req.query.fecha2);
+
+    GreenTrip.aggregate([{
+            $match: {
+                "date": {
+                    $gte: startDate,
+                    $lte: endDate
+                },
+                "state": false
+            }
+        }, {
+            $match: {
+                "date": {
+                    $gte: startDate,
+                    $lte: endDate
+                },
+                "state": false
+            }
+        }, {
+            $lookup: {
+                from: "vehicles",
+                localField: "_vehicle",
+                foreignField: "_id",
+                as: "_vehicle"
+            },
+        }, {
+            $unwind: '$_vehicle'
+        },
+        {
+            $group: {
+                _id: "$date",
+                vehicles: { $push: { _id: "$_vehicle._id", plate: "$_vehicle.plate" } },
+                cantidad: { $sum: 1 }
+            }
+        }
+    ], function(err, reports) {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error listando reportes verdes',
+                errors: err
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            preDetail: reports
+        });
+    });
+});
+
+/**
+ * CARGAR REPORTE CUADROS
+ */
+
+app.get('/:id', mdAuth.verificaToken, function(req, res) {
+
+    var id = req.params.id;
+
+    GreenTrip.findById(id, function(err, gtDB) {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar reporte cuadros',
+                errors: err
+            });
+        }
+
+        if (!gtDB) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El reporte cuadros con el id' + id + ' no existe',
+                errors: { message: 'No existe un reporte de cuadros con ese ID' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            reporte: gtDB
+        });
+    });
+});
+
+/**
+ * BORRAR REPORTE CUADROS POR ESTADO
+ */
+
+app.put('/borrar', mdAuth.verificaToken, function(req, res) {
+
+    var id = req.query.id;
+    var body = req.body;
+    var km = req.query.km;
+
+    GreenTrip.findByIdAndUpdate(id, { $set: { state: true } }, { new: true })
+        .then(function(deleteT) {
+            Vehicle.findByIdAndUpdate(body._vehicle._id, { $inc: { "km": km, "pits.$[elem].km": km } }, {
+                    multi: true,
+                    arrayFilters: [{ "elem.km": { $gte: 0 } }]
+                })
+                .then(function(vehicle) {})
+                .catch(function(err) {});
+
+            if (body._vehicle.type === 'camionG') {
+                Gondola.findByIdAndUpdate(body._vehicle._gondola, { $inc: { "pits.$[elem].km": km } }, {
+                        multi: true,
+                        arrayFilters: [{ "elem.km": { $gte: 0 } }]
+                    })
+                    .then(function(gkm) {})
+                    .catch(function(err) {});
+            }
+
+            res.status(200).json({
+                ok: true,
+                viajeV: deleteT
+            });
+        })
+        .catch(function(err) {
+            res.status(400).json({
+                ok: false,
+                mensaje: 'Error al borrar reporte cuadros',
+                errors: err
+            });
+        });
+
 });
 
 /**
@@ -137,7 +270,7 @@ app.delete('/:id', mdAuth.verificaToken, function(req, res) {
         .catch(function(err) {
             res.status(400).json({
                 ok: false,
-                mensaje: 'Error al crear reporte cuadros',
+                mensaje: 'Error al borrar reporte cuadros',
                 errors: err
             });
         });

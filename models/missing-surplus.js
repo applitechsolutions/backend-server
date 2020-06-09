7c530415114b4f9364a9109c1d5978920d101ca6
LnkColumbus <marlonveliz.nero19@gmail.com>
2020-06-09 13:32:16 -0600

    Modelo de los faltantes y sobrantes

---
+++ models/missing-surplus.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const missingSurplus = new Schema(
+  {
+    type: { type: Boolean, required: [true, 'El tipo es necesario'] },
+    _user: { type: Schema.Types.ObjectId, ref: 'User' },
+    _material: { type: Schema.Types.ObjectId, ref: 'Material' },
+    description: {
+      type: String,
+      required: [true, 'Una descripcion es necesaria'],
+    },
+    _materialCellar: { type: Schema.Types.ObjectId, ref: 'MaterialCellar' },
+    state: {
+      type: String,
+      required: [true, 'Es necesario el estado del sobrante o faltante'],
+      enum: ['pendiente', 'confirmado', 'rechazado'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('MissingSurplus', missingSurplus);
